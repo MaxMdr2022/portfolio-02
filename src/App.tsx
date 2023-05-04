c7753e18f15948aaad39a0414db03717f58bfde2
MaxMdr2022 <maxi_meder94@hotmail.com>
2023-05-04 13:29:54 -0300

    scroll2

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Route, Routes } from "react-router-dom"
 import Inicio from "./componentes/inicio/Inicio"
 import PI from "./componentes/proyectos/losProyectos/PI/PI"
 import PF from "./componentes/proyectos/losProyectos/PF/PF"
-import { ScrollRestoration } from "react-router-dom"
+
 function App() {
 
   return (
@@ -15,7 +15,7 @@ function App() {
         <Route path="/eventix" element={<PF/>}/>
         
       </Routes>
-      <ScrollRestoration />
+      
 
     </div>
   )
