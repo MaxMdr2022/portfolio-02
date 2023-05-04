4851c07188ccf1609ce3ce8ebc8d890a162901fc
MaxMdr2022 <maxi_meder94@hotmail.com>
2023-05-04 14:50:08 -0300

    browserRouter

--- src/App.tsx
+++ src/App.tsx
@@ -3,21 +3,25 @@ import Inicio from "./componentes/inicio/Inicio"
 import PI from "./componentes/proyectos/losProyectos/PI/PI"
 import PF from "./componentes/proyectos/losProyectos/PF/PF"
 import ScrollToTop from './componentes/scrollToTop/scrollToTop.tsx'
-
+import { BrowserRouter } from "react-router-dom"
 
 function App() {
 
   return (
     <div>
       
-      <ScrollToTop />
-      <Routes>
-        <Route path="/" element={<Inicio />} />
-        <Route path="/proyecto-food" element={<PI/>} />
-        <Route path="/eventix" element={<PF/>}/>
-        
-      </Routes>
+      <BrowserRouter>
       
+        <ScrollToTop />
+        <Routes>
+          <Route path="/" element={<Inicio />} />
+          <Route path="/proyecto-food" element={<PI/>} />
+          <Route path="/eventix" element={<PF/>}/>
+          
+        </Routes>
+        
+      
+      </BrowserRouter>
 
     </div>
   )
